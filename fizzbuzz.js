1f711cd5702a1f08ddb2f6baa9e97b8a6b017bcd
Preeti Mishra <preeti.mishra@softwire.com>
2025-01-20 15:19:16 +0000

    Added FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,7 +8,27 @@ function fizzbuzz() {
     // Put your code here...
     for (let num=0;num<=100;num++)
     {
-        (num%3 ==0)?  console.log('Fizz') : console.log(num);
+        const nPrint= 'Fizz'
+        if(num % 3 ==0 && num %5 ==0)
+        {
+            console.log('FizzBuzz');
+        }
+        else if(num%3 ==0)
+        {
+            console.log('Fizz');
+        }
+        else if(num%5==0)
+        {
+            console.log('Buzz');
+        }
+        else
+        {
+            console.log(num);
+        }
+        // (num%3 ==0)?  console.log('Fizz') : 
+        // (num%5 ==0)?  console.log('Buzz') : 
+        // (num%3 ==0 && num%5 ==0)? console.log('FizzBuzz') : 
+        // console.log(num);
     }
     
 }
