58683eccff0e166592f94119602df85c0b7a80e0
Preeti Mishra <preeti.mishra@softwire.com>
2025-01-21 11:28:01 +0000

    Added some stretch goals

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,29 +5,56 @@ function fizzbuzz() {
     
     console.log('Welcome to FizzBuzz!');
 
-    // Put your code here...
+        // Put your code here...
     for (let num=1;num<=100;num++)
     {
 
-        if(num % 3 ==0 && num %5 ==0)
+        var printWords = ""
+        // if(num % 3 ==0 && num %5 ==0)
+        // {
+        //     printWords += 'FizzBuzz';
+        // }
+        if(num%3 ==0)
         {
-            console.log('FizzBuzz');
+            printWords += "Fizz";
         }
-        else if(num%3 ==0)
+        if(num%5==0)
         {
-            console.log('Fizz');
+            printWords += "Buzz";
         }
-        else if(num%5==0)
+        if(num%7==0)
         {
-            console.log('Buzz');
+            printWords +="Bang";
         }
+        if(num%11==0)
+        {
+            printWords ="Bong";
+        }       
         else
         {
-            console.log(num);
+            printWords =num;
         }
-        
+        console.log(printWords);        
     }    
 }
+ // if(num%13==0)
+        // {
+        //     if(printWords.startsWith("B"))
+        //     {
+        //         printWords = "Fezz"+printWords;
+        //     }
+        //     else
+        //     {
+        //         printWords+= "Fezz"
+        //     }            
+        // }
+        // if(num%17 ==0)
+        // {
+        //     printWords = printWords.split("").splice(4).join("") + 
+        //                  printWords.split("").splice(0,4).join("")
+        // }
+
+
 
 // Now we run the main function...
 fizzbuzz();
